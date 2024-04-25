2b7099cfceda123baca016b84934794261752aa5
NeonTek <160545398+NeonTek@users.noreply.github.com>
2024-04-25 20:35:32 +0300

    Add files via upload

---
+++ scripts/load.js
@@ -0,0 +1,5 @@
+/*Psalms 37. 1 | Do not fret because of evil men or be envious of those who do wrong*/
+$(document).ready(function () {
+    $('.loader').addClass('hidden');
+    $('.content').removeClass('hidden');
+})
---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+/*Psalms 37. 1 | Do not fret because of evil men or be envious of those who do wrong*/
+$(document).ready(function () {
+
+    $('.menu').click(function() {
+        $('.overlay').toggleClass('anim');
+        $(this).addClass('open')
+    });
+
+    $('.open').click(function(){
+        $('.overlay').toggleClass('reverse-animation');
+    })
+});
+$(document).ready(function () {
+    $('.loader').addClass('hidden');
+    $('header').removeClass('hidden');
+    $('main').removeClass('hidden');
+})
