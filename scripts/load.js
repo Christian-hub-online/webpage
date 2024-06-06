bdf14ec63093b7e6a66d420a2bcf8a3326782243
NeonTek <160545398+NeonTek@users.noreply.github.com>
2024-06-06 14:25:50 +0300

    Delete scripts directory

--- scripts/load.js
+++
@@ -1,7 +0,0 @@
-/*Psalms 37. 1 | Do not fret because of evil men or be envious of those who do wrong*/
-$(document).ready(function () {
-    setTimeout(function(){
-        $('.loading').addClass('hidden');
-        $('.content').removeClass('hidden');
-    }, 2000);
-})
