c9d2c1ab7aa3f953d33c1d183973c96f2d119593
Wesley Weiss <weisswesley22@gmail.com>
2024-11-11 16:11:05 +0300

    Test

--- blogs/script.js
+++ blogs/script.js
@@ -1,13 +1,14 @@
 const previewSection = $('.blog_previews');
 
 // Fetch blog posts from API
-const data = (async => {
+const data = (async () => {
     try {
-        const response = fetch('https://neontek.co.ke/blogs/blogs.json');
-        const blog_posts = response.json();
-        console.log(blog_posts);
+        const response = await fetch('https://neontek.co.ke/blogs/blogs.json');
+        const data = await response.json();
+        return data;
     } catch (error) {
         console.error('Error fetching blog posts:', error);
     }
-})
-console.log(data)
+})();
+
+data.then(result => console.log(result));
