9833465a70bf143aef2ecd7bc61e90bd741ca4dc
Wesley Weiss <weisswesley22@gmail.com>
2024-12-04 18:29:41 +0300

    Slogans and app release system updated

---
+++ product-system/data.js
@@ -0,0 +1,10 @@
+const appData = {
+    "app-0": {
+        "name": "Magistra",
+        "version": "Version 1.0.0",
+        "description": "Keep your quiet time quiet",
+        "author": "Wesley Weiss",
+        "imgsrc": "https://neontek.co.ke/product-system/img/Magistra.png",
+        "downloadlink": "https://neontek.co.ke/assets/Magistra.apk"
+    }
+};
--- product-system/releases.js
+++ product-system/releases.js
@@ -1,15 +1,3 @@
-const appData = {
-    "app-0": {
-        "name": "Magistra",
-        "version": "Version 1.0.0",
-        "description": "Keep your quiet time quiet",
-        "author": "Wesley Weiss",
-        "imgsrc": "https://neontek.co.ke/product-system/img/Magistra.png",
-        "downloadlink": "https://neontek.co.ke/assets/Magistra.apk"
-    }
-};
-
-// Simulate a container element in the DOM
 const container = document.getElementById('container');
 
 const loadData = () => {
